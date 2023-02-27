e008ad2c14f430baa159bf1c44e3df65a704b3df
Raul Ms <raul.santos@dock.tech>
2023-02-27 16:50:09 -0300

    teste

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-docker container stop $(docker ps -a -q --filter="name=teste")
-docker container rm $(docker ps -a -q --filter="name=teste")
-docker rmi $(docker images --filter=reference="*teste:latest*" -q)
+docker stop teste 2> /dev/null || true
+docker container rm $(docker ps -a -q --filter="name=teste") 2> /dev/null || true
+docker rmi $(docker images --filter=reference="*teste:latest*" -q) 2> /dev/null || true
 docker build -t teste:latest .
 docker run -d -p 3000:3000 --name teste teste:latest
